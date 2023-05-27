4be5b7934570c7a3f53b385db0f51c8aaadb9ccc
Brett Deaton <brett.deaton@gmail.com>
2023-05-27 14:37:42 -0700

    Add qconfig tests

---
+++ tests/test_qconfig.py
@@ -0,0 +1,11 @@
+from queens.qconfig import QConfig
+import unittest
+
+
+class TestQConfig(unittest.TestCase):
+
+    def test_total_num_valid_configs(self):
+        calct = [QConfig(n).num_nonattacking_configs()
+                 for n in range(10)]
+        expct = [1, 1, 0, 0, 2, 10, 4, 40, 92, 352]
+        self.assertEqual(calct, expct)
