61c11eca6f82693dd3235a44c65112acedfc4f64
Brett Deaton <brett.deaton@gmail.com>
2023-05-27 15:21:10 -0700

    Add reference to test_qconfig

--- tests/test_qconfig.py
+++ tests/test_qconfig.py
@@ -7,5 +7,6 @@ class TestQConfig(unittest.TestCase):
     def test_total_num_valid_configs(self):
         calct = [QConfig(n).num_nonattacking_configs()
                  for n in range(10)]
+        # See https://oeis.org/A000170
         expct = [1, 1, 0, 0, 2, 10, 4, 40, 92, 352]
         self.assertEqual(calct, expct)
